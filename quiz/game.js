e7c776188c23fd2a68993818a192b03485715d4f
Jerry bony <16891252+Jbony1988@users.noreply.github.com>
2022-10-29 21:09:04 -0500

    added right and wrong class:style

--- quiz/game.js
+++ quiz/game.js
@@ -1,9 +1,9 @@
 const questionClass = "question";
 const question = document.getElementById(questionClass);
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-console.log(question, choices);
 
 let currentQuestion = {};
+// create delay after someone answers
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
@@ -45,7 +45,6 @@ startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
-  console.log(availableQuestions);
   getNewQuestion();
 };
 
@@ -58,13 +57,13 @@ getNewQuestion = () => {
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
-
   choices.forEach(choice => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
 
-  availableQuestions.splice(questionIndex + 1);
+  //   remove question so it does not show up again in list of available questions
+  availableQuestions.splice(questionIndex, 1);
   acceptingAnswers = true;
 };
 
@@ -74,8 +73,17 @@ choices.forEach(choice => {
     acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
-    console.log(selectedAnswer);
-    getNewQuestion();
+    // converted current question answer to string match string type of selected answer
+    let answerToString = currentQuestion.answer.toString();
+
+    const classToApply =
+      selectedAnswer === answerToString ? `correct` : `incorrect`;
+    console.log(classToApply);
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
   });
 });
 
